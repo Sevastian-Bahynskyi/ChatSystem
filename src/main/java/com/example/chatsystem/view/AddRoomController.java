52c611963156538fc0c44a3e880c90ffbb2731d2
Sevastian-Bahynskyi <baginski.play@gmail.com>
2023-05-11 16:35:48 +0200

    add room view and empty controller

---
+++ src/main/java/com/example/chatsystem/view/AddRoomController.java
@@ -0,0 +1,22 @@
+package com.example.chatsystem.view;
+
+import com.example.chatsystem.viewmodel.ViewModel;
+import javafx.scene.layout.Region;
+
+import java.io.IOException;
+
+public class AddRoomController implements Controller
+{
+    private Region root;
+    @Override
+    public Region getRoot()
+    {
+        return root;
+    }
+
+    @Override
+    public void init(ViewHandler viewHandler, ViewModel viewModel, Region root) throws IOException
+    {
+
+    }
+}
