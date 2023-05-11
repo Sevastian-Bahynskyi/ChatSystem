418c5e6cb78748f416a0fa2efb8682e9cf265a61
Redver <116087243+Redver@users.noreply.github.com>
2023-05-11 15:10:40 +0200

    Merged Database and UI

--- src/main/java/com/example/chatsystem/Database/DatabaseTest.java
+++ src/main/java/com/example/chatsystem/Database/DatabaseTest.java
@@ -1,5 +1,20 @@
 package com.example.chatsystem.Database;
 
+import java.sql.*;
+
 public class DatabaseTest
 {
+  public static void main(String args[]) throws SQLException
+  {
+    Driver driver = new org.postgresql.Driver();
+    DriverManager.registerDriver(driver);
+    Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sep2?currentSchema=sep2","postgres","password");
+    PreparedStatement statement = connection.prepareStatement("SELECT username FROM Chatter");
+    ResultSet rs = statement.executeQuery();
+    while(rs.next())
+    {
+      System.out.println(rs.getString("username"));
+    }
+    connection.close();
+  }
 }
