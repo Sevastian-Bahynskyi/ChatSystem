f13b0b97a7a607410034662ca47ef370b4c03fad
Maria B <331289@viauc.dk>
2023-05-15 10:57:18 +0200

    Updated Moderator DB

--- src/main/java/com/example/chatsystem/model/DatabaseManagers/ModeratorDBManager.java
+++ src/main/java/com/example/chatsystem/model/DatabaseManagers/ModeratorDBManager.java
@@ -15,30 +15,12 @@ public class ModeratorDBManager
     DriverManager.registerDriver(new org.postgresql.Driver());
   };
 
-  public void addModerator(String viaID, String username, String password){
-    try(Connection connection = getConnection())
-    {
-      PreparedStatement ps = connection.prepareStatement("insert into Chatter(VIAid, username, password, isModerator) values (?, ?, ?, true");
-      ps.setString(1, viaID);
-      ps.setString(2, username);
-      ps.setString(3, password);
-      ResultSet rs= ps.executeQuery();
-    }
-    catch (SQLException e)
-    {
-      System.err.println(e.getMessage());
-    }
-  }
-
   public void makeModerator(String viaID, String room_id){
     try(Connection connection = getConnection())
     {
-      PreparedStatement ps = connection.prepareStatement("Update chatter set ismoderator = true where viaid= ?");
-      ps.setString(1, viaID);
       PreparedStatement ps1 = connection.prepareStatement("Insert into ModeratorRoomList(room_id, moderator_id) values (? , ?)");
-      ps1.setString(1, viaID);
-      ps1.setString(2, room_id);
-      ResultSet rs = ps.executeQuery();
+      ps1.setString(1, room_id);
+      ps1.setString(2, viaID);
       ResultSet rs1 = ps1.executeQuery();
     }
     catch (SQLException e)
@@ -50,11 +32,8 @@ public class ModeratorDBManager
   public void deleteModeratorByID(String viaID){
     try(Connection connection = getConnection())
     {
-      PreparedStatement psUpdate = connection.prepareStatement("UPDATE Chatter SET ismoderator = false where viaid = ?");
       PreparedStatement psDelete = connection.prepareStatement("delete from moderatorroomlist where moderator_id = ?");
-      psUpdate.setString(1, viaID);
       psDelete.setString(1, viaID);
-      ResultSet rs1 = psUpdate.executeQuery();
       ResultSet rs2 = psDelete.executeQuery();
     }
     catch (SQLException e)
@@ -70,7 +49,7 @@ public class ModeratorDBManager
     try (Connection connection = getConnection())
     {
       PreparedStatement ps = connection.prepareStatement(
-          "SELECT viaid, username, password from chatter where viaid = ?");
+          "SELECT viaid, username, password from chatter join moderatorroommlist m on chatter.viaid = m.moderator_id where moderator_id = ?");
       ResultSet rs = ps.executeQuery();
       tempMod = new Moderator(rs.getString(1), rs.getString(2), rs.getString(3));
     }
@@ -86,7 +65,7 @@ public class ModeratorDBManager
     UserInterface tempMod = null;
     try(Connection connection = getConnection())
     {
-      PreparedStatement ps = connection.prepareStatement("SELECT viaid, username, password, ismoderator from chatter join moderatorroomlist m on chatter.viaid = m.moderator_id where room_id = ? and ismoderator = true");
+      PreparedStatement ps = connection.prepareStatement("SELECT viaid, username, password from chatter join moderatorroomlist m on chatter.viaid = m.moderator_id where room_id = ?");
       ps.setInt(1, roomId);
       ResultSet rs = ps.executeQuery();
       while (rs.next()){
