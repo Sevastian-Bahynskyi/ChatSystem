771251b5a29d8e77a1b44853da7e42d994d4bc4d
00FA12 <331305@via.dk>
2023-05-22 01:38:32 +0200

    readAllByRoomID (original method) fails. All methods tested, no exceptions caught yet

--- test/main/java/com/example/chatsystem/model/DatabaseManagers/ChatterDBManagerTest.java
+++ test/main/java/com/example/chatsystem/model/DatabaseManagers/ChatterDBManagerTest.java
@@ -16,70 +16,93 @@ public class ChatterDBManagerTest
   ChatterDBManager chatterDBManager;
 
   @BeforeEach
-  public void setUp() throws SQLException
+  public void setUp()
   {
     chatterDBManager = new ChatterDBManager();
   }
   @Test
   public void create_a_new_user_updates_DBS() throws SQLException
   {
-    Chatter test = new Chatter("123457", "test", "testPassword1");
-    Chatter get = null;
+    Chatter expected = new Chatter("123457", "test", "testPassword1");
+    Chatter result;
+    //Inserts a new Chatter into the Database
     chatterDBManager.insert("123457", "test", "testPassword1");
     try (Connection connection = getConnection())
     {
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '123457'");
+      //Tries to find the new Chatter into the Database
+      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '123457';");
       ResultSet resultSet = statement.executeQuery();
       resultSet.next();
-      get = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
+      //Stores the result of the search. No need of while loop to search through the results, as viaid's will be unique, meaning that the result of this query will always be one
+      result = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
+      //Deletes the created element so that the Database doesn't get full of testing data
+      PreparedStatement st = connection.prepareStatement("DELETE FROM Chatter WHERE viaid = '123457';");
+      st.executeUpdate();
     }
-    assertEquals(test, get);
+    assertEquals(expected, result);
   }
   @Test
   public void deletes_a_user() throws SQLException
   {
-    ArrayList<Chatter> empty = new ArrayList<>();
-    chatterDBManager.delete("123457");
-    ArrayList<Chatter> get = new ArrayList<>();
+    ArrayList<Chatter> expected = new ArrayList<>();
+    ArrayList<Chatter> result = new ArrayList<>();
     try (Connection connection = getConnection())
     {
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '123457'");
+      //Creates the element to delete
+      PreparedStatement st = connection.prepareStatement("INSERT INTO Chatter(viaid, username, password) VALUES(?,?,?);");
+      st.setString(1, "123457");
+      st.setString(2, "test");
+      st.setString(3, "PasswordTest1");
+      st.executeUpdate();
+      //Deletes the element
+      chatterDBManager.delete("123457");
+      //Tries to find the element
+      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '123457';");
       ResultSet resultSet = statement.executeQuery();
       while (resultSet.next())
       {
-      get.add(new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password")));
+        //Adds to a list the elements found
+        result.add(new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password")));
       }
     }
-    assertEquals(empty, get);
+    //Compares the results with an ArrayList that's empty, as the element should be deleted before the Select query
+    assertEquals(expected, result);
   }
 
   @Test
   public void reads_all_properly() throws SQLException
   {
-    ArrayList<UserInterface> get = (ArrayList<UserInterface>) chatterDBManager.readAll();
-    ArrayList<UserInterface> test = new ArrayList<>();
+    //Creates an ArrayList and stores the result of the readAll() method
+    ArrayList<UserInterface> expected = (ArrayList<UserInterface>) chatterDBManager.readAll();
+    ArrayList<UserInterface> result = new ArrayList<>();
     try (Connection connection = getConnection())
     {
+      //Statement to read all information from the Chatter table of the database
       PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter");
       ResultSet resultSet = statement.executeQuery();
       while (resultSet.next())
       {
-        test.add(new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password")));
+        //Adds to an empty ArrayList the results of the previous query
+        result.add(new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password")));
       }
-      assertEquals(test, get);
+      //Both ArrayLists should have the same content inside
+      assertEquals(expected, result);
     }
   }
+  //Tests the read(...) method, using the username and password to identify the user
   @Test
   public void read_by_us_pass() throws SQLException
   {
-    Chatter expected = new Chatter("111111", "Dumy_1", "password_1");
-    Chatter result;
+    //
+    Chatter result = (Chatter) chatterDBManager.read("Dumy_1", "password_1");
+    Chatter expected;
     try (Connection connection = getConnection())
     {
+      //Query to retrieve a Chatter from its username and its password
       PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE username = 'Dumy_1' AND password = 'password_1'");
       ResultSet resultSet = statement.executeQuery();
       resultSet.next();
-      result = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
+      expected = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
     }
     assertEquals(expected, result);
   }
@@ -87,16 +110,66 @@ public class ChatterDBManagerTest
   @Test
   public void read_by_viaid() throws SQLException
   {
-    Chatter expected = new Chatter("111111", "Dumy_1", "password_1");
-    Chatter result;
+    //Gets a specific chatter
+    Chatter result = (Chatter) chatterDBManager.read("111111");
+    Chatter expected;
     try (Connection connection = getConnection())
     {
+      //Statement for getting the same chatter as above
       PreparedStatement statement = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '111111'");
       ResultSet resultSet = statement.executeQuery();
       resultSet.next();
-      result = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
+      //Ideally, stores the same chatter as the DBManager stored before
+      expected = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
     }
     assertEquals(expected, result);
+  }
+  @Test
+  public void edit_Chatter() throws SQLException
+  {
+    //Stores the information that should be displayed at the end
+    Chatter expected = new Chatter("111111", "Pepe", "testPassword1");
+    Chatter result;
+    //Updates the value in the Database
+    chatterDBManager.update("111111", "Pepe", "testPassword1");
+    try (Connection connection = getConnection())
+    {
+      //Reads the information from the database, to see if the changes have been done properly
+      PreparedStatement statement1 = connection.prepareStatement("SELECT * FROM Chatter WHERE viaid = '111111'");
+      ResultSet resultSet = statement1.executeQuery();
+      resultSet.next();
+      result = new Chatter(resultSet.getString("viaid"), resultSet.getString("username"), resultSet.getString("password"));
+      //Returns to the initial value of the Database, so that the test can be repeated afterward
+      PreparedStatement statement = connection.prepareStatement("UPDATE chatter set username = 'Dumy_1', password = 'password_1' WHERE viaid = '111111'");
+      statement.executeUpdate();
+    }
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void reads_all_by_room_id() throws SQLException
+  {
+    ArrayList<UserInterface> expected = (ArrayList<UserInterface>) chatterDBManager.readAllByRoomID(1);
+    ArrayList<UserInterface> result = new ArrayList<>();
+    try (Connection connection = getConnection())
+    {
+      PreparedStatement preparedStatement = connection.prepareStatement("SELECT chatter_id FROM chatterroomlist WHERE room_id = 1;");
+      ResultSet resultSet = preparedStatement.executeQuery();
+      ArrayList<String> ids = new ArrayList<>();
+      while (resultSet.next())
+      {
+        ids.add(resultSet.getString("chatter_id"));
+      }
+      PreparedStatement statement = connection.prepareStatement("SELECT * FROM chatter WHERE viaid = ?;");
+      for (int i = 0; i < ids.size(); i++)
+      {
+        statement.setString(1, ids.get(i));
+        ResultSet resultSet1 = statement.executeQuery();
+        resultSet1.next();
+        result.add(new Chatter(resultSet1.getString("viaid"),resultSet1.getString("username"), resultSet1.getString("password")));
+      }
+      assertEquals(expected, result);
+    }
   }
   private Connection getConnection() throws SQLException
   {
