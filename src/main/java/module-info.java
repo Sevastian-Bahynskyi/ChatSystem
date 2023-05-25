6ab6fcce81e6b3519fe8bf3db4603de522c2b20c
Sevastian-Bahynskyi <baginski.play@gmail.com>
2023-05-25 13:42:26 +0200

    push

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.chatsystem {
     requires java.rmi;
     requires com.google.gson;
     requires java.sql;
+
   requires org.postgresql.jdbc;
 
     opens com.example.chatsystem to javafx.fxml;
