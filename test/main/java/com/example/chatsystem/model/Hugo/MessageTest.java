43dcd4e8ece338ded09078ac33b09979958dd756
00FA12 <331305@via.dk>
2023-05-27 21:08:26 +0200

    Testing is finished, a couple of mistakes were found

--- test/main/java/com/example/chatsystem/model/Hugo/MessageTest.java
+++ test/main/java/com/example/chatsystem/model/Hugo/MessageTest.java
@@ -9,12 +9,15 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
 
+import static org.junit.Assert.assertThrows;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class MessageTest
 {
   Message message;
   Message message2;
+  Message message3;
+  Message message4;
   UserInterface userInterface;
 
   @BeforeEach void setUp()
@@ -22,6 +25,8 @@ public class MessageTest
     userInterface = new Chatter("000000", "testChatter", "testPassword1");
     message = new Message(0, "testMessage", new Timestamp(1), userInterface, 0);
     message2 = new Message(0, "testMessage2", userInterface, 0);
+    message3 = new Message("testMessage3", userInterface, 0);
+    message4 = new Message();
   }
 
   @Test void gets_time_from_message_with_proper_format()
@@ -38,38 +43,53 @@ public class MessageTest
   {
     assertEquals(0, message.getId());
     assertEquals(0, message2.getId());
+    assertEquals(0, message3.getId());
+    //0 is the default value for an int
+    assertEquals(0, message4.getId());
   }
 
-  @Test void gets_proper_cgetMessagehannel_id()
+  @Test void gets_proper_getMessageChannel_id()
   {
     assertEquals(0, message.getChannelId());
     assertEquals(0,message2.getChannelId());
+    assertEquals(0, message3.getChannelId());
+    assertEquals(0, message4.getChannelId());
   }
 
   @Test void gets_content_of_message()
   {
     assertEquals("testMessage", message.getMessage());
     assertEquals("testMessage2", message2.getMessage());
+    assertEquals("testMessage3", message3.getMessage());
+    assertEquals("dummy", message4.getMessage());
   }
 
   @Test void changes_content_of_the_message()
   {
     message.setMessage("newMessage");
     message2.setMessage("newMessage2");
+    message3.setMessage("newMessage3");
+    message4.setMessage("newMessage4");
     assertEquals("newMessage", message.getMessage());
     assertEquals("newMessage2", message2.getMessage());
+    assertEquals("newMessage3", message3.getMessage());
+    assertEquals("newMessage4", message4.getMessage());
   }
   @Test
   void gets_the_user_that_sent_the_message()
   {
     assertEquals(userInterface, message.getUser());
     assertEquals(userInterface, message2.getUser());
+    assertEquals(userInterface, message3.getUser());
+    assertEquals(null, message4.getUser());
   }
   @Test
   void gets_id_from_user_that_sent_message()
   {
     assertEquals(userInterface.getViaId(), message.getUserId());
     assertEquals(userInterface.getViaId(), message2.getUserId());
+    assertEquals(userInterface.getViaId(), message3.getUserId());
+    assertThrows(NullPointerException.class, () -> message4.getUserId());
   }
   @Test
   void toString_returns_expected_message()
@@ -78,8 +98,13 @@ public class MessageTest
         "\nUsername: " + userInterface.getUsername() + "\nImage url: "  +  userInterface.getImageUrl();
     String expected2 = "Message: " + message2.getMessage() + "\nTime: " + message2.getTime() +
         "\nUsername: " + userInterface.getUsername() + "\nImage url: "  +  userInterface.getImageUrl();
+    String expected3 = "Message: " + message3.getMessage() + "\nTime: " + message3.getTime() +
+        "\nUsername: " + userInterface.getUsername() + "\nImage url: "  +  userInterface.getImageUrl();
+
     assertEquals(expected, message.toString());
     assertEquals(expected2, message2.toString());
+    assertEquals(expected3, message3.toString());
+    assertThrows(NullPointerException.class, () -> message4.toString());
   }
 
   @Test
@@ -87,7 +112,11 @@ public class MessageTest
   {
     String expected = userInterface.getUsername() + " sent at: " + message.getTime();
     String expected2 = userInterface.getUsername() + " sent at: " + message2.getTime();
+    String expected3 = userInterface.getUsername() + " sent at: " + message3.getTime();
+    String expected4 = null + " sent at: " + message4.getTime();
     assertEquals(expected, message.getMetadata());
     assertEquals(expected2, message2.getMetadata());
+    assertEquals(expected3, message3.getMetadata());
+    assertThrows(NullPointerException.class, () -> message4.getMetadata());
   }
 }
